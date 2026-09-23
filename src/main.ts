1c46d0bdbd944fd3f86a92da12ab05fb7f4f474f
Muhammad Umar <muhammadumarfarooqi347@gmail.com>
2026-09-23 15:05:00 +0500

    Add helmet security headers and body size cap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import 'dotenv/config';
+import 'dotenv/config';   
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as express from 'express';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(express.json({ limit: '1mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
   // Global validation: strips unknown properties (whitelist) and REJECTS
   // requests that include them (forbidNonWhitelisted). This is what makes
